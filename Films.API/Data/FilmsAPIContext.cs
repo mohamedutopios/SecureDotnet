5bc7c1a0f14385806c712967f4ee30a05b32d538
mohamedutopios <mohamed@utopios.net>
2023-09-22 09:28:49 +0200

    :tada: first commit

---
+++ Films.API/Data/FilmsAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Films.API.Model;
+
+namespace Films.API.Data
+{
+    public class FilmsAPIContext : DbContext
+    {
+        public FilmsAPIContext (DbContextOptions<FilmsAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Films.API.Model.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Films.API/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Films.API.Data;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<FilmsAPIContext>(options =>
+    options.UseInMemoryDatabase("FilmsAPIContext"));
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddAuthentication("Bearer").AddJwtBearer("Bearer", options =>
+{
+    options.Authority = "https://localhost:5005";
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = false,
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ClientIdPolicy", policy => policy.RequireClaim("client_id", "movieClient"));
+});
+
+
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    var moviesContext = serviceProvider.GetRequiredService<FilmsAPIContext>();
+    MovieContextSeed.SeedAsync(moviesContext); 
+}
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
